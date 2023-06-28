c1f86f4b216a0d422eb3e47775a075b7e2cb43d5
JasonQu <qjpcpu@gmail.com>
2023-06-28 11:41:10 +0800

    feat: regexp function names

--- extensions/regexp.go
+++ extensions/regexp.go
@@ -11,10 +11,11 @@ import (
 
 func ImportRegex(vm *glisp.Environment) error {
 	env := autoAddDoc(vm)
-	env.AddNamedFunction("regexp-compile", RegexpCompile)
-	env.AddNamedFunction("regexp-find-index", RegexpFind)
-	env.AddNamedFunction("regexp-find", RegexpFind)
-	env.AddNamedFunction("regexp-match", RegexpFind)
+	env.AddNamedFunction("regexp/compile", RegexpCompile)
+	env.AddNamedFunction("regexp/find-index", RegexpFind)
+	env.AddNamedFunction("regexp/find", RegexpFind)
+	env.AddNamedFunction("regexp/match", RegexpFind)
+	env.AddNamedFunction("regexp/replace", RegexpReplace)
 	return nil
 }
 
@@ -23,7 +24,7 @@ type SexpRegexp struct {
 }
 
 func (re *SexpRegexp) SexpString() string {
-	return fmt.Sprintf(`(regexp-compile %v)`, glisp.SexpStr(re.r.String()).SexpString())
+	return fmt.Sprintf(`(regexp/compile %v)`, glisp.SexpStr(re.r.String()).SexpString())
 }
 
 func regexpFindIndex(needle *regexp.Regexp, haystack string) (glisp.Sexp, error) {
@@ -55,23 +56,71 @@ func RegexpFind(name string) glisp.UserFunction {
 		switch t := args[0].(type) {
 		case *SexpRegexp:
 			needle = t.r
+		case glisp.SexpStr:
+			reg, err := compileRegexp(string(t))
+			if err != nil {
+				return glisp.SexpNull, err
+			}
+			needle = reg.r
 		default:
 			return glisp.SexpNull,
 				fmt.Errorf("1st argument of %v should be a compiled regular expression", name)
 		}
 
 		switch name {
-		case "regexp-find":
+		case "regexp/find":
 			str := needle.FindString(haystack)
 			return glisp.SexpStr(str), nil
-		case "regexp-find-index":
+		case "regexp/find-index":
 			return regexpFindIndex(needle, haystack)
-		case "regexp-match":
+		case "regexp/match":
 			matches := needle.MatchString(haystack)
 			return glisp.SexpBool(matches), nil
 		}
 
 		return glisp.SexpNull, errors.New("unknown function")
+	}
+}
+
+func RegexpReplace(name string) glisp.UserFunction {
+	return func(env *glisp.Environment, args []glisp.Sexp) (glisp.Sexp, error) {
+		if len(args) != 3 {
+			return glisp.WrongNumberArguments(name, len(args), 3)
+		}
+		var needle *regexp.Regexp
+		switch t := args[0].(type) {
+		case *SexpRegexp:
+			needle = t.r
+		case glisp.SexpStr:
+			reg, err := compileRegexp(string(t))
+			if err != nil {
+				return glisp.SexpNull, err
+			}
+			needle = reg.r
+		default:
+			return glisp.SexpNull,
+				fmt.Errorf("1st argument of %v should be a compiled regular expression", name)
+		}
+
+		var src string
+		switch t := args[1].(type) {
+		case glisp.SexpStr:
+			src = string(t)
+		default:
+			return glisp.SexpNull,
+				fmt.Errorf("2nd argument of %v should be a string", name)
+		}
+
+		var repl string
+		switch t := args[2].(type) {
+		case glisp.SexpStr:
+			repl = string(t)
+		default:
+			return glisp.SexpNull,
+				fmt.Errorf("3nd argument of %v should be a string", name)
+		}
+
+		return glisp.SexpStr(needle.ReplaceAllString(src, repl)), nil
 	}
 }
 
@@ -81,56 +130,62 @@ func RegexpCompile(name string) glisp.UserFunction {
 			return glisp.WrongNumberArguments(name, len(args), 1)
 		}
 
-		var re string
 		switch t := args[0].(type) {
 		case glisp.SexpStr:
-			re = string(t)
+			re, err := compileRegexp(string(t))
+			if err != nil {
+				return glisp.SexpNull, err
+			}
+			return re, nil
 		default:
 			return glisp.SexpNull,
-				errors.New("argument of regexp-compile should be a string")
+				errors.New("argument of regexp/compile should be a string")
 		}
-
-		if r, ok := regCache.Get(re); ok {
-			return glisp.Sexp(&SexpRegexp{r: r}), nil
-		}
-
-		r, err := regexp.Compile(re)
-
-		if err != nil {
-			return glisp.SexpNull, errors.New(
-				fmt.Sprintf("error during regexp-compile: '%v'", err))
-		}
-
-		regCache.Set(re, r)
-
-		return glisp.Sexp(&SexpRegexp{r: r}), nil
 	}
 }
 
+func compileRegexp(re string) (*SexpRegexp, error) {
+	if r, ok := regCache.Get(re); ok {
+		return r, nil
+	}
+
+	r, err := regexp.Compile(re)
+
+	if err != nil {
+		return nil, errors.New(
+			fmt.Sprintf("error during regexp/compile: '%v'", err))
+	}
+
+	sr := &SexpRegexp{r: r}
+	regCache.Set(re, sr)
+
+	return sr, nil
+}
+
 type regexpCache struct {
-	rmap    map[string]*regexp.Regexp
+	rmap    map[string]*SexpRegexp
 	rw      *sync.RWMutex
 	maxSize int
 }
 
-func (c *regexpCache) Get(k string) (*regexp.Regexp, bool) {
+func (c *regexpCache) Get(k string) (*SexpRegexp, bool) {
 	c.rw.RLock()
 	defer c.rw.RUnlock()
 	r, ok := c.rmap[k]
 	return r, ok
 }
 
-func (c *regexpCache) Set(k string, v *regexp.Regexp) {
+func (c *regexpCache) Set(k string, v *SexpRegexp) {
 	c.rw.Lock()
 	c.rw.Unlock()
 	if len(c.rmap) > c.maxSize {
-		c.rmap = make(map[string]*regexp.Regexp)
+		c.rmap = make(map[string]*SexpRegexp)
 	}
 	c.rmap[k] = v
 }
 
 var regCache = &regexpCache{
-	rmap:    make(map[string]*regexp.Regexp),
+	rmap:    make(map[string]*SexpRegexp),
 	rw:      new(sync.RWMutex),
 	maxSize: 100,
 }
--- tests/regexp.lisp
+++ tests/regexp.lisp
@@ -1,11 +1,28 @@
 (let* 
-     [re (regexp-compile "hello")
-      loc (regexp-find-index re "012345hello!")]
+     [re (regexp/compile "hello")
+      loc (regexp/find-index re "012345hello!")]
   (assert (= (aget loc 0) 6))
   (assert (= (aget loc 1) 11))
-  (assert (= "hello" (regexp-find re "ahellob")))
-  (assert (regexp-match re "hello"))
-  (assert (not (regexp-match re "hell"))))
+  (assert (= "hello" (regexp/find re "ahellob")))
+  (assert (regexp/match re "hello"))
+  (assert (not (regexp/match re "hell"))))
 
-(assert (str/contains?  (string (regexp-compile "hello")) "hello"))
+(assert (str/contains?  (string (regexp/compile "hello")) "hello"))
 (assert (= "(time/parse 1659438315220 'timestamp-ms)" (sexp-str (time/parse 1659438315220 'timestamp-ms))))
+
+
+(let*
+     [re  "hello"
+      loc (regexp/find-index re "012345hello!")]
+  (assert (= (aget loc 0) 6))
+  (assert (= (aget loc 1) 11))
+  (assert (= "hello" (regexp/find re "ahellob")))
+  (assert (regexp/match re "hello"))
+  (assert (not (regexp/match re "hell"))))
+
+
+(assert (= (regexp/replace (regexp/compile "a(x*)b") "-ab-axxb-" "T") "-T-T-"))
+(assert (= (regexp/replace "a(x*)b" "-ab-axxb-" "T") "-T-T-"))
+
+(assert (= (regexp/replace (regexp/compile "a(x*)b") "-ab-axxb-" "$1W") "---"))
+(assert (= (regexp/replace "a(x*)b" "-ab-axxb-" "$1W") "---"))
