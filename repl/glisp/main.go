f5afd7642cc441e7b4c129509b111ba04915e4b1
qujianping <qujianping@bytedance.com>
2023-11-30 01:08:34 +0800

    refact: repl

--- repl/glisp/main.go
+++ repl/glisp/main.go
@@ -1,33 +1,37 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"os"
 
 	"github.com/qjpcpu/glisp/repl"
 )
 
-var (
-	scriptFile  string
-	compileFile string
-	interactive bool
-)
-
 func main() {
-	flag.StringVar(&scriptFile, "f", "", "script file")
-	flag.BoolVar(&interactive, "i", false, "enter repl mode")
-	flag.StringVar(&compileFile, "c", "", "compile file")
-	flag.Parse()
-
-	if scriptFile != `` {
-		repl.RunScript(scriptFile, interactive)
-	} else if compileFile != `` {
-		if err := repl.CompileScript(compileFile); err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Println("Compile passed.")
-		}
-	} else {
+	switch len(os.Args) {
+	case 0:
+	case 1:
 		repl.Run()
+	case 2:
+		if os.Args[1] == "-i" {
+			/* glisp -i */
+			repl.Run()
+		} else {
+			/* glisp FILE */
+			file := os.Args[1]
+			os.Args = os.Args[1:]
+			repl.RunScript(file, false)
+		}
+	default:
+		if os.Args[1] == "-i" {
+			/* glisp -i FILE args... */
+			file := os.Args[2]
+			os.Args = os.Args[2:]
+			repl.RunScript(file, true)
+		} else {
+			/* glisp FILE args... */
+			file := os.Args[1]
+			os.Args = os.Args[1:]
+			repl.RunScript(file, false)
+		}
 	}
 }
